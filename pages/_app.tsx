8880d8a369f731ef652735c780020b31d14bc76e
Jacob Fink <jfink@bitwarden.com>
2022-11-23 15:29:21 -0500

    Add google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import { GoogleAnalytics } from 'nextjs-google-analytics';
 import { DefaultSeo } from 'next-seo';
 import config from '../next-seo.config';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // Set the canonical tag to the current page URL
@@ -44,6 +45,16 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <link rel='manifest' href='/site.webmanifest' />
       </Head>
+      {/* Google Tag Manager Script */}
+      <Script id='google-tag-manager' strategy='afterInteractive'>
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5W39XPX');
+      `}
+      </Script>
       <DefaultSeo {...config} />
       <GoogleAnalytics trackPageViews />
       <Layout>
