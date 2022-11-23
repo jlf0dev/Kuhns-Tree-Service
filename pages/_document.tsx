4da206229e119179fc862f8cb6afd776dadb0919
jemsusem <54963119+jemsusem@users.noreply.github.com>
2022-11-23 12:00:54 -0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,15 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5W39XPX');</script>
+<!-- End Google Tag Manager -->
+        
         {/* https://dev.to/thomasledoux1/the-best-way-to-use-google-fonts-in-next-js-tailwind-43a2
         <link
           rel="preconnect"
@@ -30,6 +39,10 @@ export default function Document() {
         </noscript> */}
       </Head>
       <body>
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5W39XPX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         <Main />
         <NextScript />
       </body>
