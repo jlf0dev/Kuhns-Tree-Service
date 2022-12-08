1c110450da19699c2ebbddb746d9d6ed190c38a3
jemsusem <sem@jemsu.com>
2022-12-08 15:37:08 -0500

    Move GTM code to _app.tsx instead of _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '@fontsource/abeezee/400.css';
 import '@fontsource/kameron/700.css';
 
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import Layout from '../components/layout';
 import Head from 'next/head';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
@@ -44,6 +45,17 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <link rel='manifest' href='/site.webmanifest' />
       </Head>
+      <Script
+        id='google-tag-manager'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5W39XPX');`,
+        }}
+      />
       <DefaultSeo {...config} />
       <GoogleAnalytics trackPageViews />
       <Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <Script
-          id='google-tag-manager'
-          strategy='afterInteractive'
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5W39XPX');`,
-          }}
-        ></Script>
-
         {/* https://dev.to/thomasledoux1/the-best-way-to-use-google-fonts-in-next-js-tailwind-43a2
         <link
           rel="preconnect"
