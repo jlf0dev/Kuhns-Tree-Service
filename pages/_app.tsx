8753285e438c92d9b8d354ea1a8671a7010e73e7
jemsusem <54963119+jemsusem@users.noreply.github.com>
2023-07-18 12:05:57 +0700

    Update GTM Code 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -46,17 +46,13 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel='manifest' href='/site.webmanifest' />
       </Head>
       {/* Google Tag Manager */}
-      <Script
-        id='google-tag-manager'
-        strategy='worker'
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5KBDLSX');`,
-        }}
-      />
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5KBDLSX');</script>
+      <!-- End Google Tag Manager -->
       <DefaultSeo {...config} />
       <GoogleAnalytics trackPageViews />
       <Layout>
